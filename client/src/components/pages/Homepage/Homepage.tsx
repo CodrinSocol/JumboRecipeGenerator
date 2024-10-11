f5127d2982580092b15dcb79f456da6652dc3c4b
CodrinSocol <codrin.socol@gmail.com>
2024-10-11 16:42:33 +0200

    asd

--- client/src/components/pages/Homepage/Homepage.tsx
+++ client/src/components/pages/Homepage/Homepage.tsx
@@ -1,7 +1,6 @@
 import {MdOutlineAccountCircle} from "react-icons/md";
 import {ListComponent} from "./ListComponent";
 
-// const melkSrc = require('%PUBLIC_URL%/Melk.png');
 const melkSrc = require("./Melk.png")
 const knokSrc = require("./rookworst.png")
 const snacks = require("./snacks.png")
@@ -9,29 +8,47 @@ const snacks = require("./snacks.png")
 export function Homepage() {
   return (
       <div className="w-full h-full flex flex-col gap-4 p-4">
-          <div className={"w-full flex flex-row items-center gap-4 "}>
-              <MdOutlineAccountCircle className={"h-10 w-10 text-black"}/>
-              <div className={"h-full flex flex-row items-end justify-end"}>
-                  Hi, Ting!
-              </div>
-          </div>
+
+          <div>3333</div>
+          {/*<div className={"w-full flex flex-row items-center gap-4 "}>*/}
+          {/*    <MdOutlineAccountCircle className={"h-10 w-10 text-black"}/>*/}
+          {/*    <div className={"h-full flex flex-row items-end justify-end"}>*/}
+          {/*        Hi, Ting!*/}
+          {/*    </div>*/}
+          {/*</div>*/}
 
           <div>
               <input type="text" placeholder={"Zoek een product"} className={"input input-primary input-ghost input-sm"}/>
           </div>
-          <div>
-              <span className={"text-black font-semibold"}>Populaire listjes</span>
-              <div className={"flex flex-row w-full h-fit gap-4"}>
-                    <ListComponent
-                        imageSrc={melkSrc}
-                        title={"Halfvolle Melk 1L"}/>
-                    <ListComponent
-                        imageSrc={knokSrc}
-                        title={"UNIX rookworst"}/>
-                    <ListComponent
-                        imageSrc={snacks}
-                        title={"Mini Snacks"}/>
+          <div className={"flex flex-col gap-8"}>
+              <div className={"flex flex-col gap-1"}>
+                  <span className={"text-black font-semibold"}>Populaire listjes</span>
+                  <div className={"flex flex-row w-full h-fit gap-4"}>
+                      <ListComponent
+                          imageSrc={melkSrc}
+                          title={"Halfvolle Melk 1L"}/>
+                      <ListComponent
+                          imageSrc={knokSrc}
+                          title={"UNIX rookworst"}/>
+                      <ListComponent
+                          imageSrc={snacks}
+                          title={"Mini Snacks"}/>
+                  </div>
+              </div>
 
+              <div className={"flex flex-col gap-1"}>
+                  <span className={"text-black font-semibold"}>Voor jouw</span>
+                  <div className={"flex flex-row w-full h-fit gap-4"}>
+                      <ListComponent
+                          imageSrc={melkSrc}
+                          title={"Halfvolle Melk 1L"}/>
+                      <ListComponent
+                          imageSrc={knokSrc}
+                          title={"UNIX rookworst"}/>
+                      <ListComponent
+                          imageSrc={snacks}
+                          title={"Mini Snacks"}/>
+                  </div>
               </div>
           </div>
       </div>
--- client/src/components/pages/Homepage/ListComponent.tsx
+++ client/src/components/pages/Homepage/ListComponent.tsx
@@ -1,4 +1,5 @@
 import {ReactElement} from "react";
+import {FaPlus} from "react-icons/fa";
 
 interface ListComponentProps {
     imageSrc: string;
@@ -8,8 +9,8 @@ interface ListComponentProps {
 export function ListComponent({imageSrc, title}: ListComponentProps): ReactElement {
 
     return (
-        <div className={"card card-compact bg-white w-48 h-54 shadow-md border-2 border-gray-300"}>
-            <div className={""}></div>
+        <div className={"group card card-compact bg-white w-48 h-54 shadow-md border-2 border-gray-300"}>
+            <div className={"hidden group-hover:flex absolute right-2 top-14 w-6 h-6 rounded-xl bg-green-400  flex-row justify-center items-center"}><FaPlus className={"text-white text-sm"}/></div>
             <figure className={"w-full h-20 bg-gray-100 flex flex-row items-center justify-center p-2"}>
                 <img src={imageSrc} alt={"card"} className={"w-full h-full object-contain"}/>
             </figure>
